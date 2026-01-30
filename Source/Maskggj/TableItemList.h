c6386c5af938a14ab3994c9420727819d16a2a59
revannn <15529726+revannn@user.noreply.gitee.com>
2026-01-30 22:54:24 +0800

    add row struct

---
+++ Source/Maskggj/TableItemList.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h" 
+#include "TableItemList.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct MASKGGJ_API FChoice {
+	GENERATED_BODY()
+	FChoice() :Intelligence(0), Charm(0), Stamina(0){}
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Intelligence;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Charm;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Stamina;
+};
+
+USTRUCT(BlueprintType)
+struct MASKGGJ_API FTableItemList : public FTableRowBase
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName Character;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Question;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText LeftChoice;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText RightChoice;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FChoice LeftModify;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FChoice RightModify;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName SFX;
+};
